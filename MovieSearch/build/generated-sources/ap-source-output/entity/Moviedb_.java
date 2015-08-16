92c5df028af2e6f966d7348248b5a3666fe348bd
zhushuai <1052785913@qq.com>
2015-08-16 22:13:50 +0800

    new version about jdk 1.7

---
+++ MovieSearch/build/generated-sources/ap-source-output/entity/Moviedb_.java
@@ -0,0 +1,20 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-16T21:53:50")
+@StaticMetamodel(Moviedb.class)
+public class Moviedb_ { 
+
+    public static volatile SingularAttribute<Moviedb, String> title;
+    public static volatile SingularAttribute<Moviedb, String> starts;
+    public static volatile SingularAttribute<Moviedb, String> description;
+    public static volatile SingularAttribute<Moviedb, String> coverurl;
+    public static volatile SingularAttribute<Moviedb, Double> rating;
+    public static volatile SingularAttribute<Moviedb, String> type;
+    public static volatile SingularAttribute<Moviedb, String> director;
+    public static volatile SingularAttribute<Moviedb, Integer> movieid;
+
+}
